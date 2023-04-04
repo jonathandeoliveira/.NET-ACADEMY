a901a3e011d786fac20e6e0eda7b8a789c6bfdc5
jonathandeoliveira <jonathan_oliveira1997@hotmail.com>
2023-04-04 19:41:00 -0300

    Corrige exercício 1

--- Exercicio1/Exercicio1/Program.cs
+++ Exercicio1/Exercicio1/Program.cs
@@ -4,10 +4,10 @@ class Program
     static void Main(string[] args)
     {
         //1) Escreva um programa em C# e no Visual Studio para calcular a média aritmética entre dois números quaisquer.
-        Console.WriteLine("Digite quatro números")
-        int n1 = Console.ReadLine();
-        int n2 = Console.ReadLine();
+        Console.WriteLine("Digite dois números");
+        int n1 = int.Parse(Console.ReadLine());
+        int n2 = int.Parse(Console.ReadLine());
         int median = (n1 + n2) / 2;
-        Console.WriteLine("A média dos números digitados é:" + median)
+        Console.WriteLine("A média dos números digitados é:" + median);
     }
 }
