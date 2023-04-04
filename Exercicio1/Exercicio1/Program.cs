777d3a68d48b7d454691cfc82b0297b2a4d5be04
jonathandeoliveira <jonathan_oliveira1997@hotmail.com>
2023-04-04 15:15:01 -0300

    Exercicios 1 e 2

--- Exercicio1/Exercicio1/Program.cs
+++ Exercicio1/Exercicio1/Program.cs
@@ -4,7 +4,10 @@ class Program
     static void Main(string[] args)
     {
         //1) Escreva um programa em C# e no Visual Studio para calcular a média aritmética entre dois números quaisquer.
-        int n1 = 18; int n2 = 20;
+        Console.WriteLine("Digite quatro números")
+        int n1 = Console.ReadLine();
+        int n2 = Console.ReadLine();
         int median = (n1 + n2) / 2;
+        Console.WriteLine("A média dos números digitados é:" + median)
     }
 }
---
+++ Exercicio2/Exercicio2/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Exercicio2;
+class Program
+{
+    static void Main(string[] args)
+    {
+        // 2)Escreva um programa em C# e no Visual Studio para calcular a média aritmética entre quatro números quaisquer.
+        Console.WriteLine("Digite quatro números")
+        int n1 = Console.ReadLine();
+        int n2 = Console.ReadLine();
+        int n3 = Console.ReadLine();
+        int n4 = Console.ReadLine();
+        int median = (n1 + n2 + n3 + n4)/4;
+        Console.WriteLine("A média dos números digitados é:" + median )
+    }
+}
