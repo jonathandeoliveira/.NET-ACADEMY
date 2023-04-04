8c8855d46756cbfaf706a7d36aae338625caf671
jonathandeoliveira <jonathan_oliveira1997@hotmail.com>
2023-04-04 14:41:57 -0300

    Exercicio 1 ok

---
+++ Exercicio1/Exercicio1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Exercicio1;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //1) Escreva um programa em C# e no Visual Studio para calcular a média aritmética entre dois números quaisquer.
+        int n1 = 18; int n2 = 20;
+        int median = (n1 + n2) / 2;
+    }
+}
