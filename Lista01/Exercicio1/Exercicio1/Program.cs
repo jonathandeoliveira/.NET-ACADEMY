ed2a3ab31464be15b93b6a2050b2bd1094b19382
jonathandeoliveira <jonathan_oliveira1997@hotmail.com>
2023-04-06 15:32:18 -0300

    Organiza pasta da lista 01

---
+++ Lista01/Exercicio1/Exercicio1/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Exercicio1;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //1) Escreva um programa em C# e no Visual Studio para calcular a média aritmética entre dois números quaisquer.
+        Console.WriteLine("Digite dois números");
+        int n1 = int.Parse(Console.ReadLine());
+        int n2 = int.Parse(Console.ReadLine());
+        int median = (n1 + n2) / 2;
+        Console.WriteLine("A média dos números digitados é:" + median);
+    }
+}
