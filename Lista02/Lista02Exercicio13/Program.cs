a20508eed85f43061fd378ed656afd6263748769
jonathandeoliveira <jonathan_oliveira1997@hotmail.com>
2023-04-06 17:30:20 -0300

    Adiciona exercício 13 da lista 02

---
+++ Lista02/Lista02Exercicio13/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Lista02Exercicio13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 13 - Tendo como dado de entrada a altura (h) de uma pessoa, construa um programa no VS que calcule 
+            seu peso ideal, utilizando as seguintes fórmulas:
+            Para homens: (72.7 * h) - 58
+            Para mulheres: (62.1 * h) - 44.7*/
+
+            double h; double peso;
+            Console.WriteLine("Olá! Digite sua altura:");
+            h = double.Parse(Console.ReadLine());
+            Console.WriteLine("Você é homem ou mulher? escolhe entre H ou M ");
+            char sex= Console.ReadLine().ToUpper()[0];
+            
+            switch (sex)
+            {
+                case 'H':
+                    peso = (72.7 * h)-58; 
+                    Console.WriteLine($"Seu peso ideal é:{peso}");
+                break;
+
+                case 'M':
+                    peso = (62.1 * h) - 44.7;
+                    Console.WriteLine($"Seu peso ideal é:{peso}");
+                break;
+
+                default:
+                    Console.WriteLine("Sexo Inválido");
+                break;
+            }
+
+        }
+    }
+}
---
+++ Lista02/Lista02Exercicio14/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Lista02Exercicio14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 14 - Faça um programa que peça o tamanho de um arquivo para download (em MB) e a velocidade de um 
+            link de Internet (em Mbps). Em seguida, calcule e informe o tempo aproximado de download 
+            do arquivo usando este link (em minutos). */
+
+            double file; double internet_vel; double download_time;
+            Console.WriteLine("Digite o tamanho do arquivo em MB:");
+            file = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite a velocidade da sua internet em Mbps");
+            internet_vel = double.Parse(Console.ReadLine());
+            download_time = (file * 8 / (internet_vel));
+
+            Console.WriteLine($"O tempo de download do seu arquivo será de: {download_time} segundos");
+
+
+        }
+    }
+}
