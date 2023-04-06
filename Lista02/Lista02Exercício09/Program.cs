9bde592240e85302c76eebdfb7dd7ff765eb85b1
jonathandeoliveira <jonathan_oliveira1997@hotmail.com>
2023-04-06 15:38:10 -0300

    Completa solução do exercicio09

--- Lista02/Lista02Exercício09/Program.cs
+++ Lista02/Lista02Exercício09/Program.cs
@@ -9,9 +9,13 @@ namespace Lista02Exercício09
             /*9 - Faça um programa no VS que receba do usuário 3 valores de glicemia em jejum, representando a medição
             diária de um paciente diabético. Para cada valor recebido analisar e informar ao paciente o seguinte:
             Se o valor diário for menor que 65, avisá-lo que corre risco de hipoglicemia
-            Se o valor diário for maior que 250, avisá-lo que corre risco de hiperglicemia.*/
+            Se o valor diário for maior que 250, avisá-lo que corre risco de hiperglicemia.
 
-            int v1; int v2; int v3;
+            Além disso, o programa deve fazer a média dos 3 valores de glicemia coletados. E se a média for 
+            menor que 80, avisá-lo que é preciso diminuir 2 unidades de insulina. Se a média for maior que
+            150, avisá-lo que será necessário adicionar 2 unidades de insulina. */
+
+            int v1; int v2; int v3; double median;
             Console.WriteLine("Digite o valor 1 da glicemia:");
             v1 = int.Parse(Console.ReadLine());
             if (v1 < 65) { Console.WriteLine("Cuidado! Risco de Hipoglicemia"); }
@@ -24,8 +28,11 @@ namespace Lista02Exercício09
 
             Console.WriteLine("Digite o valor 3 da glicemia:");
             v3 = int.Parse(Console.ReadLine());
-            if (v3 < 65) { Console.WriteLine("Cuidado! Risco de Hipoglicemia"); }
-            if (v3 > 250) { Console.WriteLine("Cuidado! Risco de Hiperclicemia"); }
+            if (v3 < 65) { Console.WriteLine("Cuidado! Risco de Hipoglicemia");}
+            if (v3 > 250) { Console.WriteLine("Cuidado! Risco de Hiperclicemia");}
+            median = (v1 + v2 + v3) / 3;
+            if (median < 80) { Console.WriteLine("Diminua duas doses de insulina");}
+            if (median > 150) { Console.WriteLine("Aumente duas doses de insulina");}
         }
     }
 }
