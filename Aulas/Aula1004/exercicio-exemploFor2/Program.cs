11767b21b833be4cbc1ffcf86ce15c6e45be895f
jonathandeoliveira <jonathan_oliveira1997@hotmail.com>
2023-04-10 16:38:46 -0300

    adiciona exercicio exemplo 2

---
+++ Aulas/Aula1004/exercicio-exemploFor2/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace exercicio_exemploFor2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*  •Escreva um algoritmo leia pelo teclado um valor inteiro chamado x, e então mostre na tela todos os números de 0 a 100, pulando de x em x. */
+            int x;
+            Console.WriteLine("Olá, digite um número");
+            x = int.Parse(Console.ReadLine());
+            Console.WriteLine('\n');
+            for (int i = 0; i <= 100; i += x) {
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}
