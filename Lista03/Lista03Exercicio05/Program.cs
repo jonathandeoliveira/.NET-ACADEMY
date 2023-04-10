eb00804776ea48b3f93715d417070a089e20d06e
jonathandeoliveira <jonathan_oliveira1997@hotmail.com>
2023-04-10 17:17:56 -0300

    adiciona exercicio 5 da lista 03

---
+++ Lista03/Lista03Exercicio05/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Lista03Exercicio05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 5 - Escreva um algoritmo para calcular o fatorial de um número. */
+            int numero; int fatorial = 1;
+            Console.WriteLine("Digite um número para saber seu fatorial");
+            numero = int.Parse(Console.ReadLine());
+
+            for (int i = 1 ; i <= numero ; i++) {
+                fatorial *= i;
+            }
+            Console.WriteLine($" O fatorial de {numero} é:{fatorial}");
+        }
+    }
+}
