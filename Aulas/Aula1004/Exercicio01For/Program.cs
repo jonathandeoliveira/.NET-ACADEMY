219c3d604e8bf455af479e9821ee2bd32a196d5b
jonathandeoliveira <jonathan_oliveira1997@hotmail.com>
2023-04-10 16:39:03 -0300

    adiciona exercicio de aula 1

---
+++ Aulas/Aula1004/Exercicio01For/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Exercicio01For
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Escreva um algoritmo para ler um número n, e em seguida ler pelo teclado a idade n pessoas, calcular a média de idade das n pessoas. */
+            double n; double idade; double total_idades = 0;
+            double media_idades;
+            Console.WriteLine("Digite um número para o total de pessoas:");
+            n = int.Parse(Console.ReadLine());
+
+            for(int i = 1 ; i <= n; i++)
+            {
+                Console.WriteLine($"Digite a idade da pessoa {i}");
+                idade = int.Parse(Console.ReadLine());
+                total_idades += idade;
+            }
+
+            media_idades = (total_idades / n);
+            Console.WriteLine($"A média de idade das {n} pessoas é: {media_idades}");
+        }
+    }
+}
