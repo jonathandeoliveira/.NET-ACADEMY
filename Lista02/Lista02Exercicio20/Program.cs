b7261232a063f509b9114d56e76f3bad6a7546aa
jonathandeoliveira <jonathan_oliveira1997@hotmail.com>
2023-04-10 17:08:07 -0300

    Adiciona exercicio 4 da lista 03

---
+++ Lista02/Lista02Exercicio20/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Lista02Exercicio20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 20 - Escreva um algoritmo para o jogo de adivinhação do número secreto. O jogador tem 3 chances e recebe dicas do tipo “é maior” ou “é menor”. 
+            plus: você pode gerar o número de forma randomica (função random c#) */
+            Random random = new Random();
+            int i = random.Next(1, 20);
+            int chances = 3;
+            
+            Console.WriteLine("Olá, vamos adivinhar o número secreto? Digite um número!");
+            Console.WriteLine()
+        }
+    }
+}
---
+++ Lista03/Lista03Exercício04/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Lista03Exercício04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 4 - Escreva um algoritmo que leia dois valores pelo teclado, x e y, e em seguida calcule a potência de x
+                    elevado na y sem utilizar a função pow. */
+            int x; int y; int result = 1;
+            Console.WriteLine("Digite um número base:");
+            x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite sua potência:");
+            y = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= y; i++)
+            {
+                result *= x;
+            }
+            Console.WriteLine($"O resultado número {x} elevado a {y} é: {result}");
+        }
+    }
+}
