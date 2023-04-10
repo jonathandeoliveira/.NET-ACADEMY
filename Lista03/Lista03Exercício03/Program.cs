6fb00784f2bbdf6d0fa1146ee21ee9a36674633d
jonathandeoliveira <jonathan_oliveira1997@hotmail.com>
2023-04-10 16:55:16 -0300

    adiciona exercicio 3

---
+++ Lista03/Lista03Exercício03/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Lista03Exercício03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*  3 - Escreva um algoritmo que leia pelo teclado um valor e então calcule a tabuada de 1 a 10 de um número
+                    x lido utilizando laços de repetição, e mostre na tela. */
+
+            Console.WriteLine("Digite um número para saber sua tabuada do 1 ao 10:");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine('\n');
+            for (int i = 1; i <= 10 ; i++) { 
+                Console.WriteLine($"{i} x {n} = {i*n} ");
+            }
+        }
+    }
+}
