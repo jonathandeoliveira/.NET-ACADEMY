879c9f5880c2492f970369a974aad505632a832c
jonathandeoliveira <jonathan_oliveira1997@hotmail.com>
2023-04-12 16:53:05 -0300

    Adiciona exercício 13 da lista 03 parte 2

---
+++ Lista03Parte02/L03P2Exercicio13/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace L03P2Exercicio13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 13. Elaborar um programa que apresente o somatório dos valores pares existentes 
+                entre 1 e 500.  */
+            int max = 500 , sum = 0;
+            for (int i = 0; i <= max; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    sum += i;
+                }
+            }
+            Console.WriteLine($"A somatória de números pares entre 1 e {max} é de: {sum}");
+        }
+    }
+}
