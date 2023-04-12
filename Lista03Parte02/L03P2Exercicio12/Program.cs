e931e37887d265695d45b77932717fba137915d4
jonathandeoliveira <jonathan_oliveira1997@hotmail.com>
2023-04-12 16:35:50 -0300

    Adiciona Lista03 Parte 2 exercício 12

---
+++ Lista03Parte02/L03P2Exercicio12/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Net;
+
+namespace L03P2Exercicio12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 12.Apresentar o total da soma dos cem primeiros números inteiros. */
+            double num, count = 100, soma = 0;
+            while (count != 0)
+            {
+                Console.WriteLine("Digite um número");
+                num = double.Parse(Console.ReadLine());
+                count--;
+                soma += num;
+            }
+            Console.WriteLine($"A soma dos {count} valores digitados é de: {soma}");
+        }
+    }
+}
