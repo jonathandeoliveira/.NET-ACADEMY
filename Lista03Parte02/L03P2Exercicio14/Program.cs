440786d454e9d492442ea0e0eed4a5da03b0eccf
jonathandeoliveira <jonathan_oliveira1997@hotmail.com>
2023-04-12 16:57:16 -0300

    Adiciona lista 03 parte 2 exercício 14

---
+++ Lista03Parte02/L03P2Exercicio14/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace L03P2Exercicio14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 14. Mostrar as potências de 2 variando de 0 a 10. */
+
+            for (int i = 0; i <= 10 ; i++)
+            {
+                double potencia = Math.Pow(2, i);
+                Console.WriteLine($"A potenciação de 2 elevado a {i} é de: {potencia}");
+            }
+        }
+    }
+}
