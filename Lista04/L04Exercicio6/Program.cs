f663db98782009652468dda3f9e48a22e56fd4bc
jonathandeoliveira <jonathan_oliveira1997@hotmail.com>
2023-04-13 17:41:05 -0300

    Adiciona exercício 7 da lista 04

---
+++ Lista04/L04Exercicio6/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace L04Exercicio6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 6.	Fazer um algoritmo que leia dez números e escreva-os na ordem contrária à ordem de leitura. Exemplo:
+                a.	lê: 		|7|40|3|9|21|0|63|31|7|22|
+                b.	escreve: 	|22|7|31|63|0|21|9|3|40|7| */
+
+             int[] vetor = new int[10];
+             int[] vetor_reverso = new int[10];
+
+             Random random = new Random();
+             for (int i = 0; i < vetor.Length; i++)
+             {
+                 vetor[i] = random.Next(1, 101);
+                 Console.WriteLine($"Valor da posição {i + 1} do vetor 1 é de {vetor[i]}");
+             }
+             Console.WriteLine("----------------------------------");
+
+             for (int i = 0 ; i < vetor.Length; i++)
+             {
+                 vetor_reverso[i] = vetor[vetor.Length - i - 1];
+                 Console.WriteLine($"Valor da posição {i+1} do vetor reverso é de {vetor_reverso[i]}");
+             }
+        }
+    }
+}
---
+++ Lista04/L04Exercicio7/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace L04Exercicio7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 7.	Fazer um algoritmo que leia dez números inteiros armazenando-os em um vetor e escreva primeiramente todos os números pares lidos e após todos os ímpares. Exemplo:
+                    a.	lê: 		|7|40|3|9|21|0|63|31|7|22|
+                    b.	escreve: 	|40|0|22|7|3|9|21|63|31|7|  */
+
+            int[] vetor = new int[10];
+            int[] vetor_ordenado = new int[10];
+            int conta_par = 0,conta_impar = 0;
+            Random random = new Random();
+            Console.WriteLine("Vetor inicial:");
+            for (int i = 0; i < vetor.Length; i++)
+            {
+                vetor[i] = random.Next(1, 101);
+                Console.Write($"|{vetor[i]}|");
+                if (vetor[i] % 2 == 0) {
+                    vetor_ordenado[conta_par] = vetor[i];
+                    conta_par++;
+                }
+                else
+                {
+                    vetor_ordenado[(vetor_ordenado.Length - 1) - conta_impar ] = vetor[i];
+                    conta_impar++;
+                }
+            }
+            Console.WriteLine(" ");
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine("Vetor ordenado:");
+            for (int i = 0; i < vetor_ordenado.Length; i++)
+            {
+                Console.Write($"|{vetor_ordenado[i]}|");
+            }
+            Console.WriteLine(" ");
+        }
+    }
+}
