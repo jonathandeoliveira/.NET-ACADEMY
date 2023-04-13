6ee941e667a795f3d96a6a746d0fbfde9ecb0d43
jonathandeoliveira <jonathan_oliveira1997@hotmail.com>
2023-04-13 15:27:15 -0300

    adiciona lista 04  exercício 03

---
+++ Lista04/L04Exercicio3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace L04Exercicio3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 3.	Escreva um algoritmo que leia os valores para um vetor de 20 elementos e 
+              então mostre na tela o índice da posição dos valores correspondentes a números primos. */
+
+            int[] vetor = new int[20];
+
+            Random random = new Random();
+            for (int i = 0; i < vetor.Length; i++)
+            {
+                vetor[i] = random.Next(1, 101);
+                Console.WriteLine($"Valor da posição {i + 1} é de {vetor[i]}");
+            }
+
+            for (int i = 0; i < vetor.Length; i++)
+            {
+                if (vetor[i] % 2 != 0 && vetor[i] % 3 != 0 && vetor[i] % 5 != 0 && vetor[i] % 7 != 0 && vetor[i] % vetor[i] == 0 ) {
+                    Console.WriteLine($"o índice {i} do vetor é um número primo com o valor de {vetor[i]} "); ; 
+                }
+            }
+            
+
+        }
+    }
+}
