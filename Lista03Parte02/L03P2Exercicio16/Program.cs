0d9d901c4881f55491dc951ed6dcf4fb848b2010
jonathandeoliveira <jonathan_oliveira1997@hotmail.com>
2023-04-13 14:52:36 -0300

    Adiciona exemplo de aula sobre vetores

---
+++ Lista03Parte02/L03P2Exercicio16/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace L03P2Exercicio16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
