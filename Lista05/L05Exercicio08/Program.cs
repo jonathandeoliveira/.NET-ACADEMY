a79a3e8b6fa2aa54a48cabd74b4c4792f9e3ca13
jonathandeoliveira <jonathan_oliveira1997@hotmail.com>
2023-04-17 15:46:55 -0300

    Adiciona Lista05 Exercicio 08

---
+++ Lista05/L05Exercicio08/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace L05Exercicio08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*   8) Ler uma matriz com 4x4 de inteiros e mostrar os números na ordem direta e inversa a que foram lidos.*/
+
+            int[,] matriz = new int[4,4];
+            Random random = new Random();
+
+            Console.WriteLine("Matriz gerada:");
+            for (int linha = 0; linha < matriz.GetLength(0); linha++)
+            {
+                for (int col = 0; col < matriz.GetLength(1); col++)
+                {
+                    matriz[linha, col] = random.Next(1, 10);
+                    Console.Write(matriz[linha, col] + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("------------------");
+
+
+            Console.WriteLine("Matriz Invertida:");
+
+            for (int linha = matriz.GetLength(0) -1 ; linha >=0; linha--)
+            {
+                for (int col = matriz.GetLength(1) -1; col >=0; col--)
+                {
+                    Console.Write(matriz[linha, col] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
