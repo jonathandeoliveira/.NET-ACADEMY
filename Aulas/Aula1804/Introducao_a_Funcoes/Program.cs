e3117fa5a579e2c7f1f633bfd9d2bbe3446d46e7
jonathandeoliveira <jonathan_oliveira1997@hotmail.com>
2023-04-18 15:17:05 -0300

    adiciona aula 18-04

---
+++ Aulas/Aula1804/Introducao_a_Funcoes/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Introducao_a_Funcoes
+{
+    internal class Program
+    {
+        public static void subtrai(double num1, double num2)
+        {
+            double resultado;
+            resultado = num1 - num2;
+            Console.WriteLine($"{num1} - {num2}=  {resultado}");
+        }
+        public static void soma(double num1, double num2)
+        {
+            double resultado;
+            resultado = num1 + num2;
+            Console.WriteLine($"{num1} + {num2}=  {resultado}");
+        }
+        public static void divide(double num1, double num2)
+        {
+            double resultado;
+            resultado = num1 / num2;
+            Console.WriteLine($"{num1} / {num2}=  {resultado}");
+        }
+        public static double multiplica(double num1, double num2)
+        {
+            double resultado;
+            resultado = num1 * num2;
+            return resultado;
+        }
+        static void Main(string[] args)
+        {
+            double n1, n2, res;
+            Console.WriteLine("Digite dois numeros: ");
+            n1 = double.Parse(Console.ReadLine());
+            n2 = double.Parse(Console.ReadLine());
+            soma(n1, n2);
+            res = multiplica(n1, n2);
+            Console.WriteLine($"{n1} *  {n2} = {res}");
+            subtrai(n1,n2);
+            divide(n1,n2);
+        }
+    }
+}
---
+++ Lista05/L05Exercicio12/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Runtime.Intrinsics.X86;
+
+namespace L05Exercicio12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 12) Escreva um programa que leia os valores de uma matriz 4x3, e em seguida mostre na tela apenas os valores cuja soma dos índices(i + j) seja um número par.*/
+
+            double[,] matrizA = new double[4, 3];
+            double[,] matrizB = new double[4, 3];
+            Random random = new Random();
+
+            // Gera matriz A //
+            Console.WriteLine("Matriz Gerada:");
+            for (int linha = 0; linha < matrizA.GetLength(0); linha++)
+            {
+                for (int col = 0; col < matrizA.GetLength(1); col++)
+                {
+                    matrizA[linha, col] = random.Next(1, 21);
+                    Console.Write(matrizA[linha, col] + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("------------------\n");
+
+            // percorrendo a matriz A  e populando a matriz B //
+            Console.WriteLine("Matriz Resultante:");
+            for (int linha = 0; linha < matrizA.GetLength(0); linha++)
+            {
+                for (int col = 0; col < matrizA.GetLength(1); col++)
+                {
+                    matrizB[linha, col] = (matrizA[linha, col] / (linha + col));
+                    if (matrizB[linha,])
+                    Console.Write(matrizB[linha, col].ToString("F2") + "\t");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
