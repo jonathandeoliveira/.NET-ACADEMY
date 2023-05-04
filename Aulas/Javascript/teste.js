7e9e3482eab04a78c412f971862fd0f914f6c830
Jonathan de Oliveira Gonçalves <jonathan_oliveira1997@hotmail.com>
2023-05-04 14:05:25 -0300

    adiciona aula sobre javascript

---
+++ Aulas/Javascript/teste.js
@@ -0,0 +1,13 @@
+var num = 10;
+var soma = 0;
+
+for (var i = 0; index < num; index++) {
+  if (i%2 == 0) {
+    console.log(i);
+  };
+}
+for (var index = 0; index < 100; index++) {
+  var soma = soma + i;
+  
+}
+console.log(soma)
