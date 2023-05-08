6bc405f1c68d4831775213854f69d456cf76f8f5
jonathandeoliveira <jonathan_oliveira1997@hotmail.com>
2023-05-08 16:35:34 -0300

    adiciona exercicio 4

---
+++ POO/Lista01/Exercicio4/Pessoa.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Runtime.Intrinsics.X86;
+using System.Text;
+using System.Threading.Tasks;
+
+    /* 4 - Crie uma classe chamada Pessoa que tenha as propriedades Nome e Idade. Utilize o encapsulamento para garantir 
+    que o nome não seja vazio e que a idade seja maior que zero. */ 
+namespace Exercicio4
+{
+    internal class Pessoa
+    {
+        private string nome;
+        private int idade;
+        public string Nome
+        {
+            get { return nome; }
+            set 
+            { 
+                if (string.IsNullOrEmpty(nome)) { Console.WriteLine("Nome não pode ser vazio"); }
+                else { nome = value; }
+            }
+        }
+
+        public int Idade  {
+            get { return idade; }
+            set 
+            {
+                if (idade > 0) { idade = value; }
+                else { Console.WriteLine("Nome não pode menor que zero"); }
+            }
+        }
+    }
+}
---
+++ POO/Lista01/Exercicio4/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Exercicio4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 4 - Crie uma classe chamada Pessoa que tenha as propriedades Nome e Idade. Utilize o encapsulamento para garantir 
+            que o nome não seja vazio e que a idade seja maior que zero. */
+
+            Pessoa p = new Pessoa();
+            Console.WriteLine("Digite seu nome");
+            p.Nome = Console.ReadLine();
+            Console.WriteLine("Digite sua idade");
+            p.Idade = int.Parse(Console.ReadLine());
+            Console.WriteLine("-------- Fim ---------");
+        }
+    }
+}
