02d4246f0e507e724e73cccc87c26bce617124ee
jonathandeoliveira <jonathan_oliveira1997@hotmail.com>
2023-05-08 17:00:36 -0300

    Adiciona exercicio 5 POO

---
+++ POO/Lista01/Exercicio5/Conta.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+/* 5 - Crie uma classe chamada Conta que tenha as propriedades Número, Saldo e Limite. Utilize o encapsulamento para 
+        garantir que o saldo nunca seja menor que zero e que o limite seja positivo e que o Número da conta não possa
+        ser alterado */
+namespace Exercicio5
+{
+    internal class Conta
+    {
+        private double limite;
+        private double saldo;
+        private int numero;
+
+        public Conta(int numero, double saldo, double limite)
+        {
+            this.numero = numero;
+            this.saldo = saldo;
+            this.limite = limite;
+        }
+
+        public double Limite 
+        { 
+            get { return limite; }
+            set 
+            { 
+                if (limite >0) { limite = value;}
+                else { Console.WriteLine("O limite deve ser um valor positivo."); }
+            } 
+        }
+
+        public double Saldo 
+        {
+            get { return saldo; }
+            set 
+            { 
+                if (saldo >= 0) { saldo = value;}
+                else { Console.WriteLine("O saldo deste cliente deve ser um valor positivo."); }
+            }
+        }
+
+        public int Numero 
+        {
+            get { return numero; }
+        }
+    }
+}
---
+++ POO/Lista01/Exercicio5/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Exercicio5
+{
+    internal class Program
+    {
+        /* 5 - Crie uma classe chamada Conta que tenha as propriedades Número, Saldo e Limite. Utilize o encapsulamento para 
+                garantir que o saldo nunca seja menor que zero e que o limite seja positivo e que o Número da conta não possa
+                ser alterado */
+        static void Main(string[] args)
+        {
+            int numero = 123456789;
+            double saldo = 1000000;
+            double limite = 5000000;
+
+            Conta c = new Conta(numero,saldo,limite);
+
+            Console.WriteLine($"Sua conta é {c.Numero}, com saldo de: ${c.Saldo} e limite de: ${c.Limite}");
+
+            Console.WriteLine("Digite um novo valor para seu saldo:");
+            c.Saldo = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite um valor para seu novo limite");
+            c.Limite = double.Parse(Console.ReadLine());
+            // c.Numero = int.Parse(Console.ReadLine()); <- não é possivel alterar pois não tem o set
+
+            Console.WriteLine($"Sua conte é {c.Numero}, com o novo saldo de: ${c.Saldo} e o novo limite de: ${c.Limite}");
+
+        }
+    }
+}
