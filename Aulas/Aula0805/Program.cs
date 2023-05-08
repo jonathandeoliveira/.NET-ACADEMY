524ca54fda38431446c859307e3d2b51f85ec073
jonathandeoliveira <jonathan_oliveira1997@hotmail.com>
2023-05-08 16:01:19 -0300

    adiciona aula 02/05 e 08/05

---
+++ Aulas/Aula0805/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Aula0805
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro c = new Carro();
+            c.Marca = "VW";
+            c.Ano = 1999;
+            c.Modelo = "Polo";
+            c.Motor = new Motor();
+            c.Motor.Cilindradas = 2000;
+       
+            while (true)
+            {
+                Console.WriteLine("Digite 1 para ligar e 2 para desligar");
+                int op = int.Parse(Console.ReadLine());
+                if (op == 1)
+                {
+                    c.LigarCarro();
+                }
+                else if (op == 2)
+                {
+                    c.DesligarCarro();
+                }
+                else if (op == 0) 
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
