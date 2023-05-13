840453c9654ec19b1c13b52f152c49af802bb874
jonathandeoliveira <jonathan_oliveira1997@hotmail.com>
2023-05-12 21:25:53 -0300

    adiciona aula do dia 12 de maio

---
+++ Aulas/Aula12-05/Program.cs
@@ -0,0 +1,10 @@
+﻿using Aula12_05;
+
+List<Pessoa> listaPessoas = new List<Pessoa>();
+
+Persistencia.lerArquivoExibeNomes("E:\\Visual Studio\\Repos\\jonathandeoliveira\\.NET-ACADEMY\\Aulas\\Aula12-05\\Dados.dat");
+
+Persistencia.popularArquivoLista("E:\\Visual Studio\\Repos\\jonathandeoliveira\\.NET-ACADEMY\\Aulas\\Aula12-05\\Dados.dat", listaPessoas);
+
+Persistencia.exibirLista(listaPessoas);
+
